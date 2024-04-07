51e221ec41451ff733277aee3808d37d9f91373a
phamvangjang <giangphamvan48@gmail.com>
2024-04-07 12:32:26 +0700

    update: handle event fillter movie

--- BookingAppUI/Admin/js/Client/index.js
+++ BookingAppUI/Admin/js/Client/index.js
@@ -1,3 +1,4 @@
+var currentdata = Object();
 // press f11 to full page view
 // vote for me at https://www.uplabs.com/posts/fluent-movie-booking
 
@@ -62,6 +63,7 @@ $('.covers').on('click', 'li', evt => {
 
 // Function to fetch movies data from the API
 function fetchMovies() {
+    // console.log('da goi api all phim');
     return fetch('http://localhost:3000/api/movies/getLists')
         .then(response => {
             if (!response.ok) {
@@ -75,24 +77,19 @@ function fetchMovies() {
         });
 }
 
-// Default to show all movies
-fetchMovies().then(data => {
-    //clear data
-    const moviesContainer = document.querySelector('.covers');
-    const moviesHTML = data.map(movie => {
-        return ` `;
-    }).join('');
-    moviesContainer.innerHTML = moviesHTML
-    renderMovies(data);
-})
-
 function renderMovies(data) {
     const moviesContainer = document.querySelector('.covers');
+    // console.log('da render ra list phim');
+
+    // Check if data is an array before using map
+    if (!Array.isArray(data)) {
+        console.error("Data is not an array");
+        return;
+    }
 
     const moviesHTML = data.map(movie => {
         const { MovieId, Banner, Name, Duration, ReleaseDate } = movie;
         const releaseDate = new Date(ReleaseDate).toLocaleDateString();
-
         return `
             <li data-index="${MovieId}">
                 <img src="${Banner}" alt="${Name}">
@@ -105,17 +102,45 @@ function renderMovies(data) {
     moviesContainer.innerHTML = moviesHTML
 }
 
+// Default to show all movies
+fetchMovies().then(data => {
+    // console.log('da goi ham renderMovie')
+    renderMovies(data);
+
+})
 
 //handle event gender film
 const handleSwitchTabFuc = function (evt) {
     $(evt.currentTarget).addClass('selected').siblings().removeClass('selected');
     var $covers = $('.covers').removeClass('loaded').removeClass('up');
     var filter = evt.currentTarget.getAttribute('data-gid');
+
     if (filter === ',') {
-        // Render all movies if "All" is selected
-        fetchMovies().then(data => {
-            renderMovies(data);
-        })
+        clearData();
+        // alert("clear");
+        fetch('http://localhost:3000/api/movies/getLists')
+            .then(response => response.json())
+            .then(data => {
+                const moviesContainer = document.querySelector('.covers');
+                // alert('da render ra list phim');
+
+                const moviesHTML = data.Movies.map(movie => {
+                    const { MovieId, Banner, Name, Duration, ReleaseDate } = movie;
+                    const releaseDate = new Date(ReleaseDate).toLocaleDateString();
+                    // console.log(data);
+
+                    return `
+                        <li data-index="${MovieId}">
+                        <img src="${Banner}" alt="${Name}">
+                        <span>${Name}</span>
+                        <small>${Duration}m ${releaseDate}</small>
+                        </li>
+                    `;
+                }).join('');
+
+                moviesContainer.innerHTML = moviesHTML
+                // renderMovies(data);
+            })
     } else {
         filterMoviesByGenreId(filter); // Call function to filter movies by GenreID
     }
@@ -142,7 +167,13 @@ function filterMoviesByGenreId(GenreId) {
             console.error('There was a problem fetching the movies:', error);
         });
 }
-
+//clear data
+function clearData() {
+    // Get the tbody element where movie data is displayed
+    const moviesContainer = document.querySelector('.covers');
+    moviesContainer.innerHTML = '';
+    // console.log('da clear movie');
+}
 
 // Function to fetch genres data from the API
 function fetchGenres() {
